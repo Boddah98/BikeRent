d58091dfdd80c21efad58b8f888ccf24013b3a21
Enzo27J <68626004+Enzo27J@users.noreply.github.com>
2023-10-01 22:17:43 -0600

    Unificación de partes
    Se actualizó el main para conectar los menús con el back end

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# BikeRent
-Esta aplicación se encarga de administrar la gestión de administración de alquiler de bicicletas utilizando el lenguaje de programación Haskell
+# Alquiler de bicicletas
+Proyecto programado 2 - Lenguajes de programación
+## Desarrolladores
+* Andy Cruz  
+* Enzo Morales
+## Proyecto a desarrollar
+Consiste en implementar una aplicación de escritorio para administrar la gestión de alquiler de bicicletas utilizando el lenguaje Haskell y la Programación Funcional.
+El programa deberá desplegar un menú al usuario con dos submenús o grupo de funciones (el menú debe mostrarse recurrentemente después de cada opción realizada -a excepción del salir- y tener la opción de subir al menú principal desde los menús secundarios) y salir.
--- main.hs
+++ main.hs
@@ -4,29 +4,72 @@ import Data.List.Split
 -- cabal update
 -- cabal install --lib split
 
-
 userDataChrg :: () -> IO [[String]] 
 userDataChrg _ = do
-    handle <- openFile "C:/Users/andya/OneDrive/Escritorio/haskell/userData.txt" ReadMode
+    handle <- openFile "userData.txt" ReadMode
     content <- hGetContents handle        
     let atributes = map (splitOn ",") (lines content)
     return atributes
 
 parkingDataChrg :: () -> IO [[String]] 
 parkingDataChrg _ = do
-    handle <- openFile "C:/Users/andya/OneDrive/Escritorio/haskell/parkingData.txt" ReadMode
+    handle <- openFile "parkingData.txt" ReadMode
     content <- hGetContents handle
     let atributes = map (splitOn ",") (lines content)    
     return atributes
 
 bikeDataChrg :: () -> IO [[String]] 
 bikeDataChrg _ = do
-    handle <- openFile "C:/Users/andya/OneDrive/Escritorio/haskell/bikeData.txt" ReadMode
+    handle <- openFile "bikeData.txt" ReadMode
     content <- hGetContents handle
     let atributes = map (splitOn ",") (lines content)    
     return atributes
 
+-- Función para mostrar el menú y obtener la selección del usuario
+showMainMenu :: IO ()
+showMainMenu = do
+    putStrLn "-> Menú Principal"
+    putStrLn "   1) Cargar y mostrar parqueos"
+    putStrLn "   2) Mostrar y asignar bicicletas"
+    putStrLn "   3) Cargar usuarios"
+    putStrLn "   4) Estadisticas"
+    putStrLn "   5) Salir"
+    putStrLn "Seleccione una opción:"
+
+-- Función principal para manejar las opciones del menú
+mainMenu :: IO ()
+mainMenu = do
+    showMainMenu
+    option <- getLine
+    putStrLn ""
+    case option of
+        "1" -> do
+            putStrLn "Has seleccionado la Opción 1."
+            mainMenu
+        "2" -> do
+            putStrLn "Has seleccionado la Opción 2."
+            mainMenu
+        "3" -> do
+            putStrLn "Has seleccionado la Opción 3."
+            mainMenu
+        "4" -> do
+            putStrLn "Has seleccionado la Opción 4."
+            mainMenu
+        "5" -> putStrLn "Programa finalizado."
+        _   -> do
+            putStrLn "\nError: Opción inválida. Por favor, selecciona una opción válida.\n"
+            mainMenu
+
 main :: IO ()
+main = do
+    --parkingData :: [[String]]
+    parkingContent <- parkingDataChrg ()
+    print parkingContent
+    putStrLn "\n¡Bienvenido al sistema de alquiler de bicletas!\n"
+    mainMenu
+
+
+{-main :: IO ()
 main = do 
     putStrLn "Prueba IO"
     userContent <- userDataChrg ()
@@ -34,4 +77,4 @@ main = do
     parkingContent <- parkingDataChrg ()
     print parkingContent
     bikeContent <- bikeDataChrg ()
-    print bikeContent 
+    print bikeContent-}
